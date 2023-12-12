af2e2350b46a9a9e44b50aae11268cb1b759671c
migavel508 <aishwinmigavel@gmail.com>
2023-12-12 18:11:46 +0530

    created

--- ecotrack-main/main.py
+++ ecotrack-main/main.py
@@ -1,26 +1,56 @@
+'''
+
+    Ecotrack - by Xgen25
+
+    
+'''
+
+
+# imports
+
 from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 from flask import json
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://aishwinmigavel:0Q2bkvFjTKiB@ep-icy-resonance-14534574-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require'
-# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+from time import gmtime,time
 from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
+
+
+# Flask app initialization
+
+app = Flask(__name__)
+
+#database configuration
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://aishwinmigavel:yiogsDCkO06x@ep-black-fire-68996719-pooler.us-east-2.aws.neon.tech/ecotrack?sslmode=require'
+
+# class for database preperation
 
 class SQLAlchemy(_BaseSQLAlchemy):
     def apply_pool_defaults(self, app, options):
         super(SQLAlchemy, self).apply_pool_defaults(self, app, options)
         options["pool_pre_ping"] = True
 
+# run database
+
 db = SQLAlchemy(app)
 
 
+# secret key
+
 app.secret_key='asdsdfsdfs13sdf_df%&'
+
+
+# Database Tables - 
+
+# User-login 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     phone_no = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     fullname = db.Column(db.String(120), nullable=False)
+
+# User-details
 
 class Details(db.Model):
     phone_no = db.Column(db.String(80), unique=True, nullable=False, primary_key = True)
@@ -31,15 +61,19 @@ class Details(db.Model):
     electricity_board = db.Column(db.String(10), nullable=True)
     Number_of_people = db.Column(db.Integer)
 
+# Applainces owned
+
 class Appliances(db.Model):
     phone_no = db.Column(db.String(80), unique=True, nullable=False,primary_key = True)
-    TV = db.Column(db.Integer,nullable=True)
+    Television = db.Column(db.Integer,nullable=True)
     Refrigerator = db.Column(db.Integer,nullable=True)
     Microwave = db.Column(db.Integer,nullable=True)
     Washing_machine = db.Column(db.Integer,nullable=True)
-    AC = db.Column(db.Integer,nullable=True)
+    Air_conditioner = db.Column(db.Integer,nullable=True)
 
-class consumer_no(db.Model):
+#Billing details
+
+class Billing_details(db.Model):
     phone_no = db.Column(db.String(80), unique=True, nullable=False,primary_key = True)
     water_consumer_no = db.Column(db.String(120), nullable=True)
     electricity_consumer_no = db.Column(db.String(120), nullable=True)
@@ -48,6 +82,7 @@ class consumer_no(db.Model):
     water_bill = db.Column(db.Integer,nullable=True)
     water_billing_date = db.Column(db.Date,nullable=True)
     water_board = db.Column(db.String(10), nullable=True)
+    electricity_billing_date = db.Column(db.Date,nullable=True)
     electricity_board = db.Column(db.String(10), nullable=True)
 
 
@@ -67,13 +102,14 @@ def register():
         firstname = request.json['firstname']
         lastname = request.json['lastname']
         fullname = firstname + " " + lastname
-        user = User.query.filter_by(phone_no=phone_no).first()
-        if user:
-            return json.dumps({'success':False}), 401, {'ContentType':'application/json'}
         user = User(phone_no=phone_no, password=password, fullname=fullname)
         user_details = Details(phone_no=phone_no,username=fullname,water_consumer_no=None, water_board=None,electricity_consumer_no=None,electricity_board=None,Number_of_people=None)
+        appliance_details = Appliances(phone_no=phone_no,Television=None,Air_conditioner=None,Refrigerator=None,Microwave=None,Washing_machine=None)
+        bill_details = Billing_details(phone_no=phone_no,water_consumer_no=None, water_board=None,electricity_consumer_no=None,electricity_board=None,electricity_billing_date=None,water_billing_date=None,water_bill=None,electricity_bill=None)
         db.session.add(user)
         db.session.add(user_details)
+        db.session.add(appliance_details)
+        db.session.add(bill_details)
         db.session.commit()
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
     return json.dumps({'success':False}), 401, {'ContentType':'application/json'}
@@ -110,24 +146,52 @@ def userdetails():
 
 @app.route("/updateuserdetails", methods=['GET','POST'])
 def updateuserdetails():
-    #if 'phone_no' in session:
+    if 'phone_no' in session:
         if request.method == 'POST':
             water_consumer_no = request.json['water_consumer_no']
             water_board = request.json['water_board']
             electricity_consumer_no = request.json['electricity_consumer_no']
             electricity_board = request.json['electricity_board']
             Number_of_people = request.json['Number_of_people']
-            #phone_no=session['phone_no'] add this instead of static its used for testing
-            user_to_update = Details.query.get_or_404("1234567899")          
+            phone_no=session['phone_no']
+            user_to_update = Details.query.get_or_404(phone_no)          
             user_to_update.water_board = water_board
             user_to_update.water_consumer_no = water_consumer_no
             user_to_update.electricity_consumer_no = electricity_consumer_no
             user_to_update.electricity_board = electricity_board
             user_to_update.Number_of_people = Number_of_people
+            details_to_update = Billing_details.query.get_or_404(phone_no)          
+            details_to_update.water_board = water_board
+            details_to_update.water_consumer_no = water_consumer_no
+            details_to_update.electricity_consumer_no = electricity_consumer_no
+            details_to_update.electricity_board = electricity_board
             db.session.commit()
             return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
         else:
-            return json.dumps({'success':False}), 200, {'ContentType':'application/json'}
+            return json.dumps({'success':False}), 401, {'ContentType':'application/json'}
+
+@app.route("/updateapplaincedetails", methods=['GET','POST'])
+def updateapplaincedetails():
+    if 'phone_no' in session:
+        if request.method == 'POST':
+            Air_conditioner = request.json['AC']
+            Television = request.json['TV']
+            Refrigerator = request.json['Refrigerator']
+            Washing_machine = request.json['Washing_machine']
+            Microwave = request.json['Microwave']
+            phone_no=session['phone_no']
+            appliance_to_update = Appliances.query.get_or_404(phone_no)          
+            appliance_to_update.Television = Television
+            appliance_to_update.Air_conditioner = Air_conditioner
+            appliance_to_update.Refrigerator = Refrigerator
+            appliance_to_update.Washing_machine = Washing_machine
+            appliance_to_update.Microwave = Microwave
+            db.session.commit()
+            return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+        else:
+            return json.dumps({'success':False}), 401, {'ContentType':'application/json'}
+
+
 
 
 if __name__ == '__main__':
